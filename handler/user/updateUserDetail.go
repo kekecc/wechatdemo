82ff84ac013199a21054cab1ac555cca4f8303a7
kk <1606295957@qq.com>
2023-04-15 16:41:58 +0800

    add api to change avatar

--- handler/user/updateUserDetail.go
+++ handler/user/updateUserDetail.go
@@ -10,18 +10,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func UpdateUserDetail(c *gin.Context) {
-	var json map[string]interface{}
-	if err := c.ShouldBindJSON(&json); err != nil {
-		log.Println("参数绑定出现问题")
-		response.Failed(c, 400, "参数绑定出现问题", err)
-		return
-	}
-	if json["name"] == nil && json["qq"] == nil && json["wx"] == nil {
-		log.Println("参数错误", json)
-		response.Failed(c, 400, "参数错误", nil)
-		return
-	}
+func UpdateUserAvatar(c *gin.Context) {
 	file, err := c.FormFile("file")
 	if err != nil {
 		log.Println(err)
@@ -34,12 +23,34 @@ func UpdateUserDetail(c *gin.Context) {
 		response.Failed(c, 400, "上传头像错误", nil)
 		return
 	}
-	json["fileid"] = file.Filename
-	defer f.Close()
+	f.Close()
 
-	if json["fileid"] != nil {
-		log.Println(json["fileid"])
+	userid := c.GetUint("user")
+	db := database.Get()
+	if userid != 0 {
+		err := db.Model(&model.User{}).Where("id = ?", userid).Update("fileid", file.Filename).Error
+		if err != nil {
+			log.Println("更改失败", err)
+			response.Failed(c, 400, "更改失败", err)
+			return
+		}
+		response.Success(c, 200, "更改成功", file.Filename)
 	}
+}
+
+func UpdateUserDetail(c *gin.Context) {
+	var json map[string]interface{}
+	if err := c.ShouldBindJSON(&json); err != nil {
+		log.Println("参数绑定出现问题")
+		response.Failed(c, 400, "参数绑定出现问题", err)
+		return
+	}
+	if json["name"] == nil && json["qq"] == nil && json["wx"] == nil {
+		log.Println("参数错误", json)
+		response.Failed(c, 400, "参数错误", nil)
+		return
+	}
+
 	userid := c.GetUint("user")
 	db := database.Get()
 	if userid != 0 {
